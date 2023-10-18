9cfa1870ebc329c42e7304c6b7e7b1406c6533fa
cyblogerz <pranavajay2002@gmail.com>
2023-10-18 23:12:41 +0530

    Added linkedin api

---
+++ third_parties/linkedin.py
@@ -0,0 +1,20 @@
+from linkedin_api import Linkedin
+from dotenv import load_dotenv
+import os
+
+def scrape_linkedin_profile(linked_in_username : str):
+    """scrape information from linkedin profiles,
+    Manually scrape info from linkedin profiles.
+    """
+    load_dotenv()
+    username = os.getenv("L_USER")
+    pwd = os.getenv("L_PASS")
+    api = Linkedin(username=username,password=pwd)
+    data = api.get_profile(linked_in_username)
+    data = {
+        k :v
+        for k,v in data.items()
+        if v not in ([],"","",None)
+            and k not in ["certifciations"]
+    }
+    return data
